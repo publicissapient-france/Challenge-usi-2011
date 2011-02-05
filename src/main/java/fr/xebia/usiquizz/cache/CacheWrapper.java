70dc8674579fbe35bac23db14f0b884d0b457c6a
Julien Buret <julien.buret@gmail.com>
2011-02-05 09:02:19 +0100

    mend

--- src/main/java/fr/xebia/usiquizz/cache/CacheWrapper.java
+++
@@ -1,7 +0,0 @@
-package fr.xebia.usiquizz.cache;
-
-public interface CacheWrapper<K, V> {
-    void put(K key, V value);
-
-    V get(K key);
-}
--- src/main/java/fr/xebia/usiquizz/core/xml/InvalidParameterFileException.java
+++
@@ -1,7 +0,0 @@
-package fr.xebia.usiquizz.core.xml;
-
-public class InvalidParameterFileException extends RuntimeException {
-    public InvalidParameterFileException(String message, Throwable throwable) {
-        super(message, throwable);
-    }
-}
