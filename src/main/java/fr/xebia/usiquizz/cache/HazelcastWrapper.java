59164108f2a28cfdd6d9035d02cbfeb3c607bfd7
Julien Buret <julien.buret@gmail.com>
2011-02-05 08:04:50 +0100

    Very basic test with Hazelcast part 2

--- src/main/java/fr/xebia/usiquizz/cache/HazelcastWrapper.java
+++ src/main/java/fr/xebia/usiquizz/cache/HazelcastWrapper.java
@@ -10,7 +10,7 @@ public class HazelcastWrapper<K, V> implements CacheWrapper<K, V> {
     private Map<K, V> mapCache;
 
     public HazelcastWrapper() {
-        client = HazelcastClient.newHazelcastClient("usi", "usi-pass", "usi");
+        client = HazelcastClient.newHazelcastClient("usi", "usi-pass", "127.0.0.1");
         mapCache = client.getMap("cache");
     }
 
