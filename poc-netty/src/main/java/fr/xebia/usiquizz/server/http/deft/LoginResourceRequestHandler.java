814a1b9d4dc5c03b49192830cd7f8dbee563e9df
Julien Buret <julien.buret@gmail.com>
2011-02-09 19:37:24 +0100

    add log

--- poc-netty/src/main/java/fr/xebia/usiquizz/server/http/deft/LoginResourceRequestHandler.java
+++ poc-netty/src/main/java/fr/xebia/usiquizz/server/http/deft/LoginResourceRequestHandler.java
@@ -77,6 +77,7 @@ public class LoginResourceRequestHandler extends RestHandler {
                         }
                         else {
                             response.setStatusCode(400);
+                            logger.info("logging failed");
                             response.finish();
                         }
                     }
@@ -89,6 +90,7 @@ public class LoginResourceRequestHandler extends RestHandler {
         }
         // ERROR
         response.setStatusCode(400);
+        logger.info("logging failed");
         response.finish();
     }
 
