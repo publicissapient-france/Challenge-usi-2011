f5877cbfc01a9859b195a73463338a3c3a0bd761
Séven Le Mesle <slemesle@xebia.fr>
2011-03-31 20:22:41 +0200

    Add equals and hashCode to joueur

--- poc-netty/src/main/java/fr/xebia/usiquizz/core/persistence/Joueur.java
+++ poc-netty/src/main/java/fr/xebia/usiquizz/core/persistence/Joueur.java
@@ -46,4 +46,28 @@ public class Joueur implements Serializable, Comparable<Joueur> {
         return comp;
     }
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Joueur joueur = (Joueur) o;
+
+        if (score != joueur.score) return false;
+        if (email != null ? !email.equals(joueur.email) : joueur.email != null) return false;
+        if (firstName != null ? !firstName.equals(joueur.firstName) : joueur.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(joueur.lastName) : joueur.lastName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = score;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
 }
