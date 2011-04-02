ce5710dab927fd1cfdfd6b5473db4b248ccb93f2
Séven Le Mesle <slemesle@xebia.fr>
2011-04-02 19:01:15 +0200

    Some Comments

--- poc-netty/src/main/java/fr/xebia/usiquizz/core/sort/RBTree.java
+++ poc-netty/src/main/java/fr/xebia/usiquizz/core/sort/RBTree.java
@@ -2,31 +2,10 @@ package fr.xebia.usiquizz.core.sort;
 
 
 
-/* Copyright (c) 2011 the authors listed at the following URL, and/or
-the authors of referenced articles or incorporated external code:
-http://en.literateprograms.org/Red-black_tree_(Java)?action=history&offset=20100112141306
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-Retrieved from: http://en.literateprograms.org/Red-black_tree_(Java)?oldid=16622
-*/
+/**
+ * Inspired from Java TreeMap with refinement to maintain MIN key, Max Key
+ * and allow to navigate the tree using NodeSet in both orders.
+ */
 
 import java.util.HashMap;
 import java.util.Map;
